a381bf69ff1d8a6ece6edb0f77b273f5bc657bf8
Anna Magdalena Bloeckner <m.bloeckner@web.de>
2020-04-29 12:59:06 +0200

    Improve server.js

--- src/server/server.js
+++ src/server/server.js
@@ -1,20 +1,20 @@
-// Setup use of environment variables
-const dotenv = require("dotenv");
-dotenv.config();
-
-// Setup empty JS object to act as endpoint for all routes
-projectData = {};
-
 // Dependencies
 const path = require("path");
 const express = require("express"); //Express to run server and routes
 const bodyParser = require("body-parser"); //Middleware
 const cors = require("cors"); // Cors for cross origin allowance
+const dotenv = require("dotenv");
 const fetch = require("node-fetch");
 //bring in the other server script files
 let getCoordinates = require("./getChoords");
 let getWeather = require("./getWeather");
 let getImage = require("./getImage");
+
+// Setup use of environment variables
+dotenv.config();
+
+// Setup empty JS object to act as endpoint for all routes
+projectData = {};
 
 // Start up an instance of the app
 const app = express();
@@ -44,7 +44,7 @@ app.get("/", function (req, res) {
   res.sendFile("dist/index.html");
 });
 
-// post request from Client
+// post route for post request from Client
 // -> assemble the data gathered by multiple API calls and store them in postDataOject
 postDataObject = {};
 app.post("/postData", function (req, res) {
